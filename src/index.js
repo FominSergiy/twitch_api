646c0b641cef90bc310b2abc95c1742f84736acb
Sergiy Fomin <sergiy.fomin@sourcedgroup.com>
2021-04-03 14:32:51 -0400

    added menu item divs - moved them into a separate component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const Row = () => {
+
+}
+
+const Selector = (props) => {
+
+
+    return (
+        <div class={"selector"}>
+            <div class="circle"></div>
+            <p>{props.text}</p>
+        </div>
+    )
+}
 
 const App = () => {
     return (
@@ -9,6 +23,9 @@ const App = () => {
             <div class="row" id="header">
                 <h1>Twitch Streamers</h1>
                 <div class="menu">
+                    <Selector text="all"></Selector>
+                    <Selector text="online"></Selector>
+                    <Selector text='offline'></Selector>
                 </div>
             </div>
         </div>
