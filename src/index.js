12b3cb382ca90efa3058f05bebf91fa0479d9c3e
Sergiy Fomin <sergiy.fomin@sourcedgroup.com>
2021-04-03 14:07:09 -0400

    removed default files. added header element - style copied from https://codepen.io/freeCodeCamp/full/Myvqmo

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import store from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+
+
+const App = () => {
+    return (
+        <div class="container">
+            <div class="row" id="header">
+                <h1>Twitch Streamers</h1>
+                <div class="menu">
+                </div>
+            </div>
+        </div>
+    )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
