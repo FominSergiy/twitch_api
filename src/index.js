05d3a355fbcc62d188b91e587722b9c69e873f8b
Sergiy Fomin <sergiy.fomin@sourcedgroup.com>
2021-04-07 10:39:57 -0400

    added useEffect hook to load data from twitch. Added a func to map the data array and return only those fields we will use when rendering each row. added a dispatch to the store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import axios from 'axios';
 
+import { store } from './reducer.js';
+import { Provider } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+
 const Row = () => {
 
 }
@@ -34,43 +38,33 @@ const Menu = () => {
 }
 
 const App = () => {
+    const dispatch = useDispatch();
 
-    async function getToken() {
-        try {
-            const response = await axios.get(
-                'https://bjnf4e2ide.execute-api.ca-central-1.amazonaws.com/default/get-twitch-bearer-token'
-            );
-            console.log(response);
-            return response;
-        } catch (error) {
-            console.error(error);
-        }
-    }
+    // run on first render only
+    React.useEffect(() => {
+        const promise = getToken()
+            .then((token) => getTopActiveStreams(token));
 
-    async function getTopGames(token) {
-        try {
-            const response = await axios.get(
-                'https://api.twitch.tv/helix/games/top',
-                {
-                    headers: {
-                        'Authorization': `${token}`,
-                        'Client-Id': '8nxrw92890jbiwzodkdlcfh70wvgqv'
-                    }
-                }
-            );
-            console.log(response);
-            // return response;
-        } catch (error) {
-            console.error(error);
-        }
+        promise
+            .then(data => {
+                const filteredData = data.map(r => processRow(r));
+                console.log(filteredData);
+                dispatch({
+                    type: 'ADD ACTIVE STREAMS',
+                    streams: filteredData
+                });
+            })
+            .catch(err => console.log(err))
 
-    }
+    }, []);
+
 
     const cl = () => {
+        //! CONTINUE HERE - NEED TO WORK WITH THIS PROMISE
         // getToken().then((res) => console.log(res));
-        getToken()
-            .then((res) => getTopGames(res['data']));
-
+        const data = getToken()
+            .then((res) => getTopActiveStreams(res));
+        console.dir(data)
     }
 
     return (
@@ -85,6 +79,68 @@ const App = () => {
 }
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
+
+
+//? SUPPORTING FUNCTIONS
+
+async function getToken() {
+
+    if (localStorage.getItem("token") !== null) {
+        const token = localStorage.getItem("token");
+        console.log('token returned from local storage!');
+        return token;
+    }
+
+    try {
+        const response = await axios.get(
+            'https://bjnf4e2ide.execute-api.ca-central-1.amazonaws.com/default/get-twitch-bearer-token'
+        );
+        localStorage.setItem("token", response['data']);
+        console.log(response['data']);
+        return response['data'];
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function getTopActiveStreams(token) {
+    try {
+        const response = await axios.get(
+            'https://api.twitch.tv/helix/streams/',
+            {
+                headers: {
+                    'Authorization': `${token}`,
+                    'Client-Id': '8nxrw92890jbiwzodkdlcfh70wvgqv'
+                }
+            }
+        );
+        // console.log(response);
+        return response['data']['data']
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const processRow = (row) => {
+    // process each row returned by the twitch streams api
+
+    // thumb url width ahd height
+    const width = 50;
+    const height = 50;
+
+
+    const thumb_url = row.thumbnail_url.substr(1, row.thumbnail_url.search("{") - 1);
+
+    return {
+        online: true,
+        game_id: row.game_id,
+        thumbnail_url: thumb_url + `${width}x${height}.jpg`,
+        title: row.title,
+        url: `https://www.twitch.tv/${row.user_login}`
+    };
+}
